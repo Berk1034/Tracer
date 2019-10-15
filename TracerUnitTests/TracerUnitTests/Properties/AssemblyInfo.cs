d7f024e4cea28fa224e18def89a67bdf5549552f
Constantin Hitov <kostya.khitov@mail.ru>
2019-10-16 00:19:19 +0300

    Add TracerUnitTests filled with basic test

---
+++ TracerUnitTests/TracerUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TracerUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TracerUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c77e9bb1-ecb7-4ae6-abde-1cd803336482")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TracerUnitTests/TracerUnitTests/TracerUnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TracerLibrary;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace TracerUnitTests
+{
+    public class A
+    {
+        private Tracer _tracer;
+        private B _innerClassB;
+        public A(Tracer tracer)
+        {
+            this._tracer = tracer;
+            _innerClassB = new B(_tracer);
+        }
+        public void MethodA()
+        {
+            _tracer.StartTrace();
+
+            _innerClassB.MethodBWithCycle();
+
+            _tracer.StopTrace();
+        }
+
+        public void MultiThreadedFoo()
+        {
+            _tracer.StartTrace();
+            B b = new B(_tracer);
+
+            Thread thread1 = new Thread(() => { b.MethodBWithCycle(); });
+
+            thread1.Start();
+            _tracer.StopTrace();
+        }
+    }
+
+    public class B
+    {
+        private Tracer _tracer;
+        private List<int> TestInt;
+        public B(Tracer tracer)
+        {
+            this._tracer = tracer;
+            TestInt = new List<int>();
+        }
+        public void MethodBWithCycle()
+        {
+            _tracer.StartTrace();
+            for (int i = 0; i < 1000; i++)
+            {
+                TestInt.Add(i * 4);
+            }
+            _tracer.StopTrace();
+        }
+    }
+
+    [TestClass]
+    public class TracerUnitTest1
+    {
+        [TestMethod]
+        public void ShouldTraceOneThread()
+        {
+            Tracer tracer = new Tracer();
+            A a = new A(tracer);
+            a.MethodA();
+            Assert.AreEqual(1, tracer.GetTraceResult().Threads.Count);
+        }
+    }
+}
