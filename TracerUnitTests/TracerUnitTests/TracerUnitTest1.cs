d5a3ececf2cac33b1513e974daeafdd6b759f4d1
Constantin Hitov <kostya.khitov@mail.ru>
2019-10-16 14:16:41 +0300

    Add some tests

--- TracerUnitTests/TracerUnitTests/TracerUnitTest1.cs
+++ TracerUnitTests/TracerUnitTests/TracerUnitTest1.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TracerLibrary;
 using System.Collections.Generic;
 using System.Threading;
+using System.Linq;
 
 namespace TracerUnitTests
 {
@@ -24,7 +25,7 @@ namespace TracerUnitTests
             _tracer.StopTrace();
         }
 
-        public void MultiThreadedFoo()
+        public void MultiThreadedMethodA()
         {
             _tracer.StartTrace();
             B b = new B(_tracer);
@@ -56,6 +57,74 @@ namespace TracerUnitTests
         }
     }
 
+    public class C
+    {
+        private Tracer _tracer;
+        private List<int> TestInt;
+        private D _innerClassD;
+        public C(Tracer tracer)
+        {
+            this._tracer = tracer;
+            TestInt = new List<int>();
+            _innerClassD = new D(_tracer);
+        }
+
+        public void MultiThreadedMethodC()
+        {
+            _tracer.StartTrace();
+            D d = new D(_tracer);
+            Thread thread1 = new Thread(() => { d.MethodDWithCycle(); });
+            Thread thread2 = new Thread(() => { MethodCWithCycle(); });
+
+            thread1.Start();
+            thread2.Start();
+            for(int i = 1; i < 1000; i++) {
+                MethodCWithCycle();
+                TestInt.Clear();
+            }
+
+
+            _tracer.StopTrace();
+        }
+
+        public void MethodCWithCycle()
+        {
+            _tracer.StartTrace();
+            for (int i = 0; i < 1000; i++)
+            {
+                TestInt.Add(i * 5);
+            }
+            _tracer.StopTrace();
+        }
+    }
+
+    public class D
+    {
+        private Tracer _tracer;
+        private List<int> TestInt;
+        public D(Tracer tracer)
+        {
+            this._tracer = tracer;
+            TestInt = new List<int>();
+        }
+        public void MethodDWithCycle()
+        {
+            _tracer.StartTrace();
+            for (int i = 0; i < 1000; i++)
+            {
+                TestInt.Add(i * 4);
+            }
+            InnerMethodD();
+            _tracer.StopTrace();
+        }
+        public void InnerMethodD()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(100);
+            _tracer.StopTrace();
+        }
+    }
+
     [TestClass]
     public class TracerUnitTest1
     {
@@ -66,6 +135,66 @@ namespace TracerUnitTests
             A a = new A(tracer);
             a.MethodA();
             Assert.AreEqual(1, tracer.GetTraceResult().Threads.Count);
+        }
+
+        [TestMethod]
+        public void ShouldContainCorrectThreadID()
+        {
+            Tracer tracer = new Tracer();
+            A a = new A(tracer);
+            a.MethodA();
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            Assert.AreEqual(true, tracer.GetTraceResult().Threads.Keys.Contains(threadId));
+        }
+
+        [TestMethod]
+        public void ShouldTraceTwoMethods()
+        {
+            Tracer tracer = new Tracer();
+            A a = new A(tracer);
+            a.MethodA();
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            Assert.AreEqual(2, tracer.GetTraceResult().Threads.Values.ElementAt(0).MethodsCount);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFilledDictionary()
+        {
+            Tracer tracer = new Tracer();
+            A a = new A(tracer);
+            a.MultiThreadedMethodA();
+            Assert.AreEqual(1, tracer.GetTraceResult().Threads.Count);
+        }
+
+        [TestMethod]
+        public void ShouldReturnEqualTimeForThreadAndAllItsMethods()
+        {
+            Tracer tracer = new Tracer();
+            A a = new A(tracer);
+            a.MultiThreadedMethodA();
+
+            foreach(KeyValuePair<int, ThreadTraceResult> thread in tracer.GetTraceResult().Threads)
+            {
+                Assert.AreEqual((int)Math.Floor(thread.Value.WorkTime), tracer.GetTraceResult().GetSummOfMethodsWorkTimes(thread.Key));
+            }
+        }
+
+        [TestMethod]
+        public void ShouldTraceOneInnerMethod()
+        {
+            Tracer tracer = new Tracer();
+            C c = new C(tracer);
+            c.MultiThreadedMethodC();
+            Assert.AreEqual(1, tracer.GetTraceResult().Threads.Values.ElementAt(0).InnerMethods.Count);
+        }
+
+        [TestMethod]
+        public void ShouldTraceThreeThreads()
+        {
+            Tracer tracer = new Tracer();
+            C c = new C(tracer);
+            c.MultiThreadedMethodC();
+            Assert.AreEqual(3, tracer.GetTraceResult().Threads.Count);
         }
     }
 }
